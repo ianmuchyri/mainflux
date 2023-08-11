28958e9f35fa92bc5a1c31bf6f3ebec74b0b73ab
ianmuchyri <ianmuchiri8@gmail.com>
2023-08-11 15:46:55 +0300

    Remove first slash
    Signed-off-by: ianmuchyri <ianmuchiri8@gmail.com>

--- pkg/sdk/go/bootstrap.go
+++ pkg/sdk/go/bootstrap.go
@@ -14,11 +14,11 @@ import (
 )
 
 const (
-	configsEndpoint        = "/things/configs"
-	bootstrapEndpoint      = "/things/bootstrap"
-	whitelistEndpoint      = "/things/state"
-	bootstrapCertsEndpoint = "/things/configs/certs"
-	bootstrapConnEndpoint  = "/things/configs/connections"
+	configsEndpoint        = "things/configs"
+	bootstrapEndpoint      = "things/bootstrap"
+	whitelistEndpoint      = "things/state"
+	bootstrapCertsEndpoint = "things/configs/certs"
+	bootstrapConnEndpoint  = "things/configs/connections"
 	secureEndpoint         = "secure"
 )
 
